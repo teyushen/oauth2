2151ba88d87caed3fa5a24215d3bf40a1273d01c
Dennis <teyushen@gmail.com>
2017-06-10 00:56:54 +0800

    first commit

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ### 啟動
 
 ```
+$ cd authorization-server
 $ mvn spring-boot:run
 ```
 
@@ -15,11 +16,34 @@ insert into oauth_client_details (client_id, client_secret, scope, authorized_gr
 ```
 ### 取得 code
 
-[http://localhost:8888/oauth2-server/oauth/authorize?response_type=code&client_id=sample&redirect_uri=http://example.com](http://localhost:8888/oauth2-server/oauth/authorize?response_type=code&client_id=sample&redirect_uri=http://example.com)
+[http://localhost:8888/authorization-server/oauth/authorize?response_type=code&client_id=sample&redirect_uri=http://example.com](http://localhost:8888/authorization-server/oauth/authorize?response_type=code&client_id=sample&redirect_uri=http://example.com)
 
 ### 用 code 換 token
 
 ```
-$ curl sample:samplesecret@localhost:8888/oauth2-server/oauth/token -d grant_type=authorization_code -d client_id=sample -d redirect_uri=http://example.com -d code=mKkwZK 
-=> code 換成自己取得的
-``
+$ curl sample:samplesecret@localhost:8888/authorization-server/oauth/token -d grant_type=authorization_code -d client_id=sample -d redirect_uri=http://example.com -d code=kd1Rg1
+(code 換成自己取得的)
+```
+
+### 利用 token 取得 user 資料
+```
+$ TOKEN=f8d33487-b560-4564-adb3-ea88de7e49dd
+$ curl -H "Authorization: Bearer $TOKEN" -v localhost:8888/authorization-server/user
+```
+
+
+## resource-server
+
+### 啟動
+
+```
+$ cd resource-server
+$ mvn spring-boot:run
+```
+
+### 對 resource server 發送請求
+
+```
+$ TOKEN=f8d33487-b560-4564-adb3-ea88de7e49dd
+$ curl -H "Authorization: Bearer $TOKEN" -v localhost:9999/resource-server/
+```
--- resource-server/src/main/java/sample/Application.java
+++ resource-server/src/main/java/sample/Application.java
@@ -1,31 +1,37 @@
 package sample;
 
 import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableResourceServer
 public class Application {
-	private static final Log log = LogFactory.getLog(Application.class);
 
 	public static void main(String[] args) throws Exception {
 		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
 	}
 
-	
-	@RequestMapping("/user")
-    public Principal user(Principal user) {
-        log.debug("user: " + user.toString());
-        return user;
-    }
-	
+	private String message = "Hello world!";
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public Map<String, String> home() {
+		return Collections.singletonMap("message", message);
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public Map<String, String> user(Principal user) {
+		return Collections.singletonMap("message", "user is: " + user.toString());
+	}
+
 }
