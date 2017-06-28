de00be059a55f2a9e4bb463d69dafed52d3367fd
Dennis <teyushen@gmail.com>
2017-06-28 23:41:24 +0800

    modify import

--- resource-server/src/main/java/sample/ResourceApplication.java
+++ resource-server/src/main/java/sample/ResourceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
