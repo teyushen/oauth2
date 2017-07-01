f7ed5d98097993982435198b9d963fa8c024d8cb
Dennis <teyushen@gmail.com>
2017-07-01 08:21:59 +0800

    modify /user

--- authorization-server/src/main/java/sample/AuthorizationApplication.java
+++ authorization-server/src/main/java/sample/AuthorizationApplication.java
@@ -1,12 +1,22 @@
 package sample;
 
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableResourceServer
 public class AuthorizationApplication {
 	private static final Log log = LogFactory.getLog(AuthorizationApplication.class);
 
@@ -14,6 +24,9 @@ public class AuthorizationApplication {
 		ConfigurableApplicationContext ctx = SpringApplication.run(AuthorizationApplication.class, args);
 	}
 
-	
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public Map<String, String> user(Principal user) {
+		return Collections.singletonMap("message", "user is: " + user.toString());
+	}
 
 }
